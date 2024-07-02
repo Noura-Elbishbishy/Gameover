45fefe7d58249379193c8ba798190e0e6dd34027
Noura-Elbishbishy <nouraelbishbishy@gmail.com>
2024-07-02 12:55:22 +0300

    first commit

--- js/details.js
+++ js/details.js
@@ -1,43 +1,108 @@
-const searchParams = location.search; 
-const params = new URLSearchParams(searchParams);
-const id = params.get("id"); 
+// const searchParams = location.search; 
+// const params = new URLSearchParams(searchParams);
+// const id = params.get("id"); 
 
-let containerDetails = {};
-(async function () {
-   const options = {
-      method: "GET",
-      headers: {
-         "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
-         "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-      },
-   };
-   const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
-   const responseData = await api.json();
-   containerDetails = responseData;
-   displayData();
-   console.log(responseData);
-})();
+// let containerDetails = {};
+// (async function () {
+//    const options = {
+//       method: "GET",
+//       headers: {
+//          "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
+//          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+//       },
+//    };
+//    const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
+//    const responseData = await api.json();
+//    containerDetails = responseData;
+//    displayData();
+//    console.log(responseData);
+// })();
 
-function displayData() {
-   const cartona = `
-   <div class="col-md-4">
-   <figure>
-      <img src="${containerDetails.thumbnail}" class="w-100" alt="details image" />
-   </figure>
-</div>
-<div class="col-md-8">
-   <div>
-      <nav aria-label="breadcrumb">
-         <ol class="breadcrumb" class="text-light">
-            <li class="breadcrumb-item text-reset"><a href="./home.html">Home</a></li>
-            <li class="breadcrumb-item text-info" aria-current="page">${containerDetails.title}</li>
-         </ol>
-      </nav>
-      <h1>${containerDetails.title}</h1>
-      <h3>About ${containerDetails.title}</h3>
-      <p>${containerDetails.description}</p>
-   </div>
-</div>
-   `;
-   document.getElementById("detailsData").innerHTML = cartona;
+// function displayData() {
+//    const cartona = `
+//    <div class="col-md-4">
+//    <figure>
+//       <img src="${containerDetails.thumbnail}" class="w-100" alt="details image" />
+//    </figure>
+// </div>
+// <div class="col-md-8">
+//    <div>
+//       <nav aria-label="breadcrumb">
+//          <ol class="breadcrumb" class="text-light">
+//             <li class="breadcrumb-item text-reset"><a href="./home.html">Home</a></li>
+//             <li class="breadcrumb-item text-info" aria-current="page">${containerDetails.title}</li>
+//          </ol>
+//       </nav>
+//       <h1>${containerDetails.title}</h1>
+//       <h3>About ${containerDetails.title}</h3>
+//       <p>${containerDetails.description}</p>
+//    </div>
+// </div>
+//    `;
+//    document.getElementById("detailsData").innerHTML = cartona;
+// }
+
+
+// nfs al codes bs turned into Class 
+class GameDetails {
+   constructor() {
+       this.containerDetails = {};
+       this.init();
+   }
+
+   init() {
+       const searchParams = location.search;
+       const params = new URLSearchParams(searchParams);
+       const id = params.get("id");
+
+       if (id) {
+           this.fetchGameDetails(id);
+       }
+   }
+
+   async fetchGameDetails(id) {
+       const options = {
+           method: "GET",
+           headers: {
+               "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
+               "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+           },
+       };
+
+       const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
+       const responseData = await api.json();
+       this.containerDetails = responseData;
+       UI.displayData(this.containerDetails);
+       console.log(responseData);
+   }
 }
+
+class UI {
+   static displayData(containerDetails) {
+       const cartona = `
+           <div class="col-md-4">
+               <figure>
+                   <img src="${containerDetails.thumbnail}" class="w-100" alt="details image" />
+               </figure>
+           </div>
+           <div class="col-md-8">
+               <div>
+                   <nav aria-label="breadcrumb">
+                       <ol class="breadcrumb text-light">
+                           <li class="breadcrumb-item text-reset"><a href="./home.html">Home</a></li>
+                           <li class="breadcrumb-item text-info" aria-current="page">${containerDetails.title}</li>
+                       </ol>
+                   </nav>
+                   <h1>${containerDetails.title}</h1>
+                   <h3>About ${containerDetails.title}</h3>
+                   <p>${containerDetails.description}</p>
+               </div>
+           </div>
+       `;
+       document.getElementById("detailsData").innerHTML = cartona;
+   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+   new GameDetails();
+});
--- js/main.js
+++ js/main.js
@@ -1,63 +1,140 @@
-const links = document.querySelectorAll(".menu a");
-let gamesData = [];
-const loading = document.querySelector(".loading");
-getGames("mmorpg");
+// const links = document.querySelectorAll(".menu a");
+// let gamesData = [];
+// const loading = document.querySelector(".loading");
+// getGames("mmorpg");
 
-for (let i = 0; i < links.length; i++) { //3lshan al navbar
-   links[i].addEventListener("click", function (e) {
-      document.querySelector(".menu .active").classList.remove("active");
-      links[i].classList.add("active");
-      const category = e.target.innerText; 
-      console.log(category); 
-      getGames(category); 
-   });
+// for (let i = 0; i < links.length; i++) { //3lshan al navbar
+//    links[i].addEventListener("click", function (e) {
+//       document.querySelector(".menu .active").classList.remove("active");
+//       links[i].classList.add("active");
+//       const category = e.target.innerText; 
+//       console.log(category); 
+//       getGames(category); 
+//    });
+// }
+// async function getGames(categoryName) {
+//    loading.classList.remove("d-none"); 
+//    const options = {
+//       method: "GET",
+//       headers: {
+//          "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
+//          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+//       },
+//    };
+//    const apiResponse = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${categoryName}`, options);
+//    const data = await apiResponse.json();
+//    gamesData = data;
+//    displayData();
+//    loading.classList.add("d-none"); // a-Hide al loading
+// }
+// function showDetails(id) {
+//     location.href = `./details.html?id=${id}`;
+//  }
+// function displayData() {
+//    let gamesBox = ``;
+//    for (let i = 0; i < gamesData.length; i++) {
+//      gamesBox += `
+//       <div class="col">
+//       <div onmouseleave="stopVideo(event)" onmouseenter="startVideo(event)" onclick="showDetails(${gamesData[i].id})" class="card h-100 bg-transparent" role="button" >
+//          <div class="card-body">
+//             <figure class="position-relative">
+//                <img class="card-img-top object-fit-cover h-100" src="${gamesData[i].thumbnail}" />
+//             </figure>
+//             <figcaption>
+//                <div class="hstack justify-content-between">
+//                   <h3 class="h6 small"> ${gamesData[i].title} </h3>
+//                   <span class="badge text-bg-primary p-2">Free</span>
+//                </div>
+//                <p class="card-text small text-center opacity-50">
+//                   ${gamesData[i].short_description}
+//                </p>
+//             </figcaption>
+//          </div>
+//          <footer class="card-footer small hstack justify-content-between">
+//             <span class="badge badge-color">${gamesData[i].genre}</span>
+//             <span class="badge badge-color">${gamesData[i].platform}</span>
+//          </footer>
+//       </div>
+//    </div>
+//       `;}
+//    document.getElementById("gameData").innerHTML = gamesBox;
+// }
+
+//  nfs al code using classes 
+class GameApp {
+   constructor() {
+       this.gamesData = [];
+       this.loading = document.querySelector(".loading");
+       this.links = document.querySelectorAll(".menu a");
+       this.init();
+   }
+
+   init() {
+       this.getGames("mmorpg");
+
+       for (let i = 0; i < this.links.length; i++) {
+           this.links[i].addEventListener("click", (e) => {
+               document.querySelector(".menu .active").classList.remove("active");
+               this.links[i].classList.add("active");
+               const category = e.target.innerText;
+               console.log(category);
+               this.getGames(category);
+           });
+       }
+   }
+
+   async getGames(categoryName) {
+       this.loading.classList.remove("d-none");
+       const options = {
+           method: "GET",
+           headers: {
+               "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
+               "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+           },
+       };
+       const apiResponse = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${categoryName}`, options);
+       const data = await apiResponse.json();
+       this.gamesData = data;
+       UI.displayData(this.gamesData);
+       this.loading.classList.add("d-none");
+   }
+
+   showDetails(id) {
+       location.href = `./details.html?id=${id}`;
+   }
 }
-async function getGames(categoryName) {
-   loading.classList.remove("d-none"); 
-   const options = {
-      method: "GET",
-      headers: {
-         "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
-         "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-      },
-   };
-   const apiResponse = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${categoryName}`, options);
-   const data = await apiResponse.json();
-   gamesData = data;
-   displayData();
-   loading.classList.add("d-none"); // a-Hide al loading
-}
-function showDetails(id) {
-    location.href = `./details.html?id=${id}`;
- }
-function displayData() {
-   let gamesBox = ``;
-   for (let i = 0; i < gamesData.length; i++) {
-     gamesBox += `
-      <div class="col">
-      <div onmouseleave="stopVideo(event)" onmouseenter="startVideo(event)" onclick="showDetails(${gamesData[i].id})" class="card h-100 bg-transparent" role="button" >
-         <div class="card-body">
-            <figure class="position-relative">
-               <img class="card-img-top object-fit-cover h-100" src="${gamesData[i].thumbnail}" />
-            </figure>
-            <figcaption>
-               <div class="hstack justify-content-between">
-                  <h3 class="h6 small"> ${gamesData[i].title} </h3>
-                  <span class="badge text-bg-primary p-2">Free</span>
+
+class UI {
+   static displayData(gamesData) {
+       let gamesBox = ``;
+       for (let i = 0; i < gamesData.length; i++) {
+           gamesBox += `
+               <div class="col">
+                   <div onmouseleave="stopVideo(event)" onmouseenter="startVideo(event)" onclick="app.showDetails(${gamesData[i].id})" class="card h-100 bg-transparent" role="button" >
+                       <div class="card-body">
+                           <figure class="position-relative">
+                               <img class="card-img-top object-fit-cover h-100" src="${gamesData[i].thumbnail}" />
+                           </figure>
+                           <figcaption>
+                               <div class="hstack justify-content-between">
+                                   <h3 class="h6 small"> ${gamesData[i].title} </h3>
+                                   <span class="badge text-bg-primary p-2">Free</span>
+                               </div>
+                               <p class="card-text small text-center opacity-50">
+                                   ${gamesData[i].short_description}
+                               </p>
+                           </figcaption>
+                       </div>
+                       <footer class="card-footer small hstack justify-content-between">
+                           <span class="badge badge-color">${gamesData[i].genre}</span>
+                           <span class="badge badge-color">${gamesData[i].platform}</span>
+                       </footer>
+                   </div>
                </div>
-               <p class="card-text small text-center opacity-50">
-                  ${gamesData[i].short_description}
-               </p>
-            </figcaption>
-         </div>
-         <footer class="card-footer small hstack justify-content-between">
-            <span class="badge badge-color">${gamesData[i].genre}</span>
-            <span class="badge badge-color">${gamesData[i].platform}</span>
-         </footer>
-      </div>
-   </div>
-      `;}
-   document.getElementById("gameData").innerHTML = gamesBox;
+           `;
+       }
+       document.getElementById("gameData").innerHTML = gamesBox;
+   }
 }
 
-
+const app = new GameApp();
